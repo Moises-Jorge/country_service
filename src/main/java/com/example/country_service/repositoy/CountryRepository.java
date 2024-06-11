c66c9c9310e72ac9d4289d1531cc7a830991c9eb
Moisés Jorge <moisesjorge30@gmail.com>
2024-06-11 11:30:43 +0100

    added annotation of repository in countryRepository

--- src/main/java/com/example/country_service/repositoy/CountryRepository.java
+++ src/main/java/com/example/country_service/repositoy/CountryRepository.java
@@ -1,9 +1,11 @@
 package com.example.country_service.repositoy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.country_service.model.Country;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
     Country findByName(String name);
 }
